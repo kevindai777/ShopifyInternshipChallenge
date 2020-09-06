4d5c294c42e50b43df2bdd1a7541059933108ba6
Kevin Dai <adamgordon@KevinDai.home>
2020-09-05 21:18:50 -0400

    Updates

---
+++ precache-manifest.83e4d31ec8ec90377ae35c78335421b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be65cbb23983778ed426809a81ee63a3",
+    "url": "/ShopifyInternshipChallenge/index.html"
+  },
+  {
+    "revision": "0d6f92e32bdabd6c695b",
+    "url": "/ShopifyInternshipChallenge/static/css/main.46af9a89.chunk.css"
+  },
+  {
+    "revision": "70d7280f24b518c70b54",
+    "url": "/ShopifyInternshipChallenge/static/js/2.a7129804.chunk.js"
+  },
+  {
+    "revision": "254c01d1a3c7d8124adf4c35e46b76e3",
+    "url": "/ShopifyInternshipChallenge/static/js/2.a7129804.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d6f92e32bdabd6c695b",
+    "url": "/ShopifyInternshipChallenge/static/js/main.d3e0eef3.chunk.js"
+  },
+  {
+    "revision": "207f31ad2ec9e707409d",
+    "url": "/ShopifyInternshipChallenge/static/js/runtime-main.abf07e27.js"
+  },
+  {
+    "revision": "b95376410738065506c03faa2e493fb4",
+    "url": "/ShopifyInternshipChallenge/static/media/noimage.b9537641.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7e8e69689ed28efbff982bd76b1faf17.js"
+  "/ShopifyInternshipChallenge/precache-manifest.83e4d31ec8ec90377ae35c78335421b3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ShopifyInternshipChallenge/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
